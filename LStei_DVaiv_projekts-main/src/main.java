06a012becbe885b85aba90ffe5543f3e6b76cd36
23DP1DVaiv <166118264+23DP1DVaiv@users.noreply.github.com>
2025-04-21 21:10:41 +0300

    Update main.java

--- LStei_DVaiv_projekts-main/src/main.java
+++ LStei_DVaiv_projekts-main/src/main.java
@@ -9,100 +9,102 @@ import services.UserService;
 
 public class main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        AlbumService albumService = new AlbumService();
-        RatingService ratingService = new RatingService();
-        UserService userService = new UserService();
+        try (Scanner scanner = new Scanner(System.in)) {
+            AlbumService albumService = new AlbumService();
+            RatingService ratingService = new RatingService();
+            UserService userService = new UserService();
 
-        System.out.println("Welcome to Sonium!");
-        System.out.print("Enter your name: ");
-        String username = scanner.nextLine();
+            System.out.println("Welcome to Sonium!");
+            System.out.print("Enter your name: ");
+            String username = scanner.nextLine();
 
-        // Регистрация пользователя
-        User currentUser = userService.registerUser(username);
-        System.out.println("Hi, " + currentUser.getUsername() + "!");
+            // Регистрация 
+            User currentUser = userService.registerUser(username);
+            System.out.println("Hi, " + currentUser.getUsername() + "!");
 
-        while (true) {
-            // Главное меню
-            System.out.println("Menu");
-            System.out.println("1. Find an album");
-            System.out.println("2. My albums");
-            System.out.println("3. Quit");
+            while (true) {
+                // Главное меню
+                System.out.println("Menu");
+                System.out.println("1. Find an album");
+                System.out.println("2. My albums");
+                System.out.println("3. Quit");
 
-            System.out.print("Choose: ");
-            String choice = scanner.nextLine();
+                System.out.print("Choose: ");
+                String choice = scanner.nextLine();
 
-            switch (choice) {
-                case "1":
-                    System.out.print("Enter album's name: ");
-                    String query = scanner.nextLine();
-                    List<Album> foundAlbums = albumService.searchAlbums(query);
+                switch (choice) {
+                    case "1" -> {
+                        System.out.print("Enter album's name: ");
+                        String query = scanner.nextLine();
+                        List<Album> foundAlbums = albumService.searchAlbums(query);
 
-                    if (foundAlbums.isEmpty()) {
-                        System.out.println("Nothing found.");
-                        break;
-                    }
-
-                    // Отобразим найденные альбомы
-                    for (int i = 0; i < foundAlbums.size(); i++) {
-                        System.out.println((i + 1) + ". " + foundAlbums.get(i));
-                    }
-
-                    System.out.print("Choose album by number: ");
-                    int albumIndex = Integer.parseInt(scanner.nextLine()) - 1;
-                    Album selectedAlbum = foundAlbums.get(albumIndex);
-
-                    System.out.println("You chose: " + selectedAlbum);
-                    System.out.println("1. mark as listened");
-                    System.out.println("2. Rate an album");
-                    System.out.println("3. View average rate");
-
-                    String action = scanner.nextLine();
-
-                    switch (action) {
-                        case "1":
-                            ratingService.markAsListened(currentUser.getId(), selectedAlbum.getId());
-                            System.out.println("Marked as listened!");
+                        if (foundAlbums.isEmpty()) {
+                            System.out.println("Nothing found.");
                             break;
-                        case "2":
-                            System.out.print("Enter rating (1-5): ");
-                            int score = Integer.parseInt(scanner.nextLine());
-                            ratingService.rateAlbum(currentUser.getId(), selectedAlbum.getId(), score);
-                            System.out.println("Thanks for your rating!");
-                            break;
-                        case "3":
-                            double avgRating = ratingService.getAlbumAverageRating(selectedAlbum.getId());
-                            System.out.println("Average album's rating: " + avgRating);
-                            break;
-                        default:
-                            System.out.println("Incorrect choise.");
-                    }
-                    break;
+                        }
 
-                case "2":
-                    List<Rating> listened = ratingService.getUserListenedAlbums(currentUser.getId());
-                    if (listened.isEmpty()) {
-                        System.out.println("You haven't listened any albums yet.");
-                    } else {
-                        System.out.println("Your listened albums:");
-                        for (Rating r : listened) {
-                            Album a = albumService.getAlbumById(r.getAlbumId());
-                            String info = a.getTitle() + " (" + a.getArtist() + ")";
-                            if (r.getRating() > 0) {
-                                info += " - Rating: " + r.getRating();
-                            }
-                            System.out.println(". " + info);
+                        // Результат поиска
+                        for (int i = 0; i < foundAlbums.size(); i++) {
+                            System.out.println((i + 1) + ". " + foundAlbums.get(i));
+                        }
+
+                        System.out.print("Choose album by number: ");
+                        int albumIndex = Integer.parseInt(scanner.nextLine()) - 1;
+                        Album selectedAlbum = foundAlbums.get(albumIndex);
+
+                        System.out.println("You chose: " + selectedAlbum);
+                        System.out.println("1. Mark as listened");
+                        System.out.println("2. Rate an album");
+                        System.out.println("3. View average rating");
+
+                        String action = scanner.nextLine();
+
+                        switch (action) {
+                            case "1" -> {
+                                ratingService.markAsListened(currentUser.getId(), selectedAlbum.getId());
+                                System.out.println("Marked as listened!");
+                        }
+                            case "2" -> {
+                                System.out.print("Enter rating (1-5): ");
+                                int score = Integer.parseInt(scanner.nextLine());
+                                ratingService.rateAlbum(currentUser.getId(), selectedAlbum.getId(), score);
+                                System.out.println("Thanks for your rating!");
+                        }
+                            case "3" -> {
+                                double avgRating = ratingService.getAlbumAverageRating(selectedAlbum.getId());
+                                System.out.println("Average album's rating: " + avgRating);
+                        }
+                            default -> System.out.println("Incorrect choise.");
                         }
                     }
-                    break;
 
-                case "3":
-                    System.out.println("Bye-bye!!!");
-                    return;
+                    case "2" -> {
+                        List<Rating> listened = ratingService.getUserListenedAlbums(currentUser.getId());
+                        if (listened.isEmpty()) {
+                            System.out.println("You haven't listened any albums yet.");
+                        } else {
+                            System.out.println("Your logged albums:");
+                            for (Rating r : listened) {
+                                Album a = albumService.getAlbumById(r.getAlbumId());
+                                String info = a.getTitle() + " (" + a.getArtist() + ")";
+                                if (r.getRating() > 0) {
+                                    info += " - Rating: " + r.getRating();
+                                }
+                                System.out.println(". " + info);
+                            }
+                        }
+                    }
 
-                default:
-                    System.out.println("Incorrect. Try again.");
+                    case "3" -> {
+                        System.out.println("Bye-bye!!!");
+                        return;
+                    }
+
+                    default -> System.out.println("Incorrect. Try again.");
+                }
             }
+        } catch (NumberFormatException e) {
+
         }
     }
 }
